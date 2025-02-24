a54bda3cee6c458f040f0b8daa51444211efcdda
M.Horvath.extern@transnetbw.de <m.horvath.extern@transnetbw.de>
2025-02-24 09:02:15 +0100

    plugin extraction

---
+++ apps/plugins/src/menu/CompasImportFromApi.ts
@@ -0,0 +1,23 @@
+import { html, LitElement } from 'lit-element';
+import { get } from 'lit-translate';
+
+import '@openscd/open-scd/src/filtered-list.js';
+import '@openscd/open-scd/src/wizard-textfield.js';
+import { newWizardEvent, Wizard } from '@openscd/open-scd/src/foundation.js';
+
+import '../compas/CompasImportFromApi.js';
+
+export default class ImportFromApiPlugin extends LitElement {
+  private importFromApiWizard(): Wizard {
+    return [
+      {
+        title: get('compas.import.title'),
+        content: [html`<compas-import-from-api></compas-import-from-api>`],
+      },
+    ];
+  }
+
+  async run(): Promise<void> {
+    this.dispatchEvent(newWizardEvent(this.importFromApiWizard()));
+  }
+}
