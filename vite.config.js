db31fd0465fe686dc846339937790440dedb9e15
philippilievskibearingpointcom <philipp.ilievski@bearingpoint.com>
2025-06-04 14:59:55 +0200

    change vite build process

--- vite.config.js
+++ vite.config.js
@@ -13,41 +13,14 @@ export default defineConfig({
     },
     build: {
         lib: {
-            entry: path.resolve(__dirname, 'apps/plugins/src/editors/Substation.ts'),
-            name: 'Substation',
-            fileName: 'substation',
+            entry: path.resolve(__dirname, 'apps/plugins/src/editors/Sitipe.ts'),
+            name: 'Sitipe',
+            fileName: 'sitipe',
             formats: ['es']
         },
         rollupOptions: {
-            external: [
-                'lit',
-                'lit-html',
-                'lit-element',
-                '@material/mwc-fab',
-                '@material/mwc-dialog',
-                '@material/mwc-button',
-                "@material/mwc-formfield",
-                "@material/mwc-icon",
-                "@material/mwc-icon-button",
-                "@material/mwc-icon-button-toggle",
-                "@material/mwc-list",
-                "@material/mwc-menu",
-                "@material/mwc-select",
-                "@material/mwc-switch",
-                "@material/mwc-textarea",
-                "@material/mwc-textfield",
-                "@material/mwc-linear-progress",
-                "@material/mwc-tab",
-                "@material/mwc-tab-bar",
-                "@material/mwc-top-app-bar-fixed",
-            ],
             output: {
                 inlineDynamicImports: true,
-                globals: {
-                    lit: 'lit',
-                    'lit-html': 'litHtml',
-                    'lit-element': 'LitElement'
-                }
             }
         },
         target: 'esnext'
